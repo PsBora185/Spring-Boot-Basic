05db872bec859f31e566f03527aa5dd3d74c73e8
pranav <pranavsinghbora@gmail.com>
2026-02-26 16:14:40 +0530

    Get all users

--- RestApi/second/src/main/java/com/example/second/controller/UserController.java
+++ RestApi/second/src/main/java/com/example/second/controller/UserController.java
@@ -1,11 +1,26 @@
 package com.example.second.controller;
 
+import com.example.second.entity.User;
+import com.example.second.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
 }
--- RestApi/second/src/main/java/com/example/second/service/UserService.java
+++ RestApi/second/src/main/java/com/example/second/service/UserService.java
@@ -1,14 +1,26 @@
 package com.example.second.service;
 
 import com.example.second.entity.User;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class UserService {
 
-    private Map<String , User> userDb = new HashMap<>();
+    private Map<String, User> userDb = new HashMap<>();
 
+    @PostConstruct
+    public void initDb(){
+        userDb.put("1", new User(1, "Joe", "joe@gmail.com"));
+        userDb.put("2", new User(2, "Jenny", "jenny@gmail.com"));
+    }
+
+    public List<User> getUsers() {
+        return new ArrayList<>(userDb.values());
+    }
 }
